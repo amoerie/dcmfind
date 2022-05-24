30153a551346a058a73c0a643ae75ab46a2e077e
Alexander Moerman <alexander.moerman@gmail.com>
2022-05-24 16:26:28 +0200

    Update publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,10 @@
+$nugetUrl = $env:NUGET_URL
+$nugetApiKey = $env:NUGET_API_KEY
+
+if(-not $nugetUrl) {
+    $nugetUrl = "https://api.nuget.org/v3/index.json";
+}
+
 $projectName = "DcmFind"
 $projectPath = Resolve-Path (Join-Path $PSScriptRoot "./$projectName/")
 $csProjPath = Resolve-Path (Join-Path $projectPath "$projectName.csproj")
@@ -10,9 +17,16 @@ Write-Host "Packing version $version"
 
 dotnet pack $csprojPath --configuration Release
 
-$nupkgFile = Resolve-Path (Join-Path "$projectPath/nupkg" "$projectName.$version.nupkg")
+$nupkgFile = Resolve-Path (Join-Path "$projectPath/bin/Release" "$projectName.$version.nupkg")
 
 Write-Host "Publishing NuGet package file"
 
-nuget push $nupkgFile -skipduplicate -source NuGet
-nuget push $nupkgFile -skipduplicate -source Github
+if($nugetApiKey)
+{
+    nuget push $nupkgFile -skipduplicate -source $nugetUrl -apikey $nugetApiKey
+} 
+else 
+{
+    # API key is presumed to be preconfigured
+    nuget push $nupkgFile -skipduplicate -source $nugetUrl
+}
